aa584bf2f89834b4c64d6f1a8dbc44f7935f4afd
FixRay001 <bogdanzakharov2@gmail.com>
2025-11-09 20:46:22 +0200

    move
    Removed the approximate_pi function and adjusted return statement.

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -7,16 +7,4 @@ def move(my_list, direction):
     elif direction == "left" and index_of_one > 0:
         new_list[index_of_one], new_list[index_of_one - 1] = 0, 1
 
-    return new_list
-
-def approximate_pi(n_terms):
-
-    pi_approx = 0
-    for n in range(n_terms):
-        term = (-1) ** n / (2 * n + 1)
-        pi_approx += term
-        current_pi = 4 * pi_approx
-        print("Term", n+1, "Approximation:", current_pi)
-
-    return 4 * pi_approx
-    
+    return new_list    
